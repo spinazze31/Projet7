e2d308d771741e7e2693ab50b3e547e8fe7530e3
Audrey Spinazzé <spinazze31@hotmail.fr>
2024-10-29 08:37:37 +0100

    modification url

--- src/Router/url.jsx
+++ src/Router/url.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import Home from "../Pages/home.jsx";
 import About from "../Pages/about.jsx";
 import ErrorPage from "../Pages/errorpage.jsx";
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 import "../src/Main_scss/main.css";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router } from "react-router-dom";
 import Header from "./Layout/header.jsx";
 import Footer from "./Layout/footer.jsx";
 import Url from "./Router/url.jsx";
